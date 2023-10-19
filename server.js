9f4b9d6d27337b76619ed9fb0ba4dfdf703f82a1
toocels <toocels@gmail.com>
2023-10-20 00:10:58 +0530

    new connections and delete connections workign

--- server.js
+++ server.js
@@ -14,16 +14,19 @@ const server = app.listen(PORT, ADDRESS, _ => {
 var wss = new WebSocket.Server({ server })
 var clients = []
 var games = 0
+var idd = 10;
 
 wss.on('connection', (ws) => {
 	console.log("[ WS ] New connection")
 
 	// add client to database
-	var uniq_id = Date.now()
+	// var uniq_id = Date.now()
+	var uniq_id = idd++;
 	clients.push({ con: ws, stat: undefined, id: uniq_id })
 	ws.send(uniq_id)
+
 	// check if game available for client
-	var a = findFriendFor(wss, uniq_id)
+	var a = findFriendFor(ws)
 	if (a != undefined) {
 		console.log("Match between:" + uniq_id + ' ' + a.id + ' game:' + games)
 		getClient(uniq_id).stat = games
@@ -33,51 +36,38 @@ wss.on('connection', (ws) => {
 
 	ws.on('error', console.error)
 	ws.on('message', (data) => {
-		console.log(data.toString())
+		// console.log(data.toString())
 	})
 
-	ws.on('close', (ws) => {
+	ws.on('close', _ => {
 		console.log("[ WS ] A connection was closed")
-		removeAlonePlayer(ws)
-		printAll()
+
+		// remove connected client and partnet
+		var a = getClient(ws, "con")
+		console.log("game ended for", a['id'])
+		var st = a['stat'];
+		clients.splice(clients.indexOf(a), 1);
+		var b = getClient(st, "stat")
+		if (b != undefined)
+			b.con.close()
+
 	})
 })
 
-function removeAlonePlayer(a) {
-	for (var i in clients) {
-		console.log(clients[i].con == a)
-		console.log(clients[i].isAlive)
-		console.log(a.isAlive)
-	}
-	// for (var i in clients) {
-	// 	if (clients[i + 1] != undefined)
-	// 		if (clients[i].stat != clients[i + 1].stat) {
-	// 			clients.pop(clients[i])
-	// 			console.log(clients[i].id)
-	// 		}
-	// 	i++
-	// }
-}
-
 function findFriendFor(wss, uniq_id) {
-	// for (var i in clients)
-	// 	if (clients[i].stat == undefined && clients[i].id != uniq_id)
-	// 		return clients[i]
-	// return undefined
 	if (clients.length > 1 && clients[clients.length - 2].stat == undefined)
 		return clients[clients.length - 2]
 	return undefined
 }
 
-function getClient(uniq_id) {
+function getClient(ident, parm = "id") {
 	for (var i in clients)
-		if (clients[i].id == uniq_id)
+		if (clients[i][parm] == ident)
 			return clients[i]
 	return undefined
 }
 
 function printAll() {
-	for (var i in clients) {
+	for (var i in clients)
 		console.log(clients[i].id, clients[i].stat)
-	}
 }
