cd4f59846672a5732368d9e1c7d5ca2c7fb90e7a
toocels <toocels@gmail.com>
2023-10-19 20:06:53 +0530

    msg back n fourth

--- public/script.js
+++ public/script.js
@@ -1,1 +1,9 @@
-console.log("Sup")
+const socket = new WebSocket("ws://"+window.location.hostname);
+
+socket.addEventListener("open", (event) => {
+  socket.send("New customer here!");
+});
+
+socket.addEventListener("message", (event) => {
+  console.log("Message from server ", event.data);
+});
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const app = express();
-
+const WebSocket = require('ws');
 const PORT = 80;
 const ADDRESS = "localhost";
+const URL = ADDRESS + ':' + PORT;
 
 app.use(express.static('public'));
+const server = app.listen(PORT, ADDRESS, _ => {
+	console.log("Server listening on " + URL);
+});
 
-app.listen(PORT, ADDRESS, _ => {
-    console.log("Server listening on " + ADDRESS + ':' + PORT);
+var wss = new WebSocket.Server({ server })
+
+wss.on('connection', (ws) => {
+	console.log("New client!")
+	ws.send('something');
+
+	ws.on('error', console.error);
+
+	ws.on('message', (data) => {
+		console.log('received: %s', data);
+		ws.send('somethingelse');
+	});
 });
