f09ec3fae43a4bf7e2882cef96136346a8c23148
toocels <toocels@gmail.com>
2023-10-21 17:00:23 +0530

    starting on frontned, server good kinda

--- public/script.js
+++ public/script.js
@@ -1,8 +1,23 @@
 const socket = new WebSocket("ws://" + window.location.hostname);
-
-socket.addEventListener("open", (event) => {
-});
+socket.addEventListener("open", (event) => {});
 
 socket.addEventListener("message", (event) => {
-	console.log("Message from server ", event.data);
+	msg = JSON.parse(event.data)
+
+	switch (msg['purpose']) {
+		case "game_found":
+			console.log("Playing game number:", msg['data']['game'])
+			console.log("Playing symbol:", msg['data']['you_are'])
+
+			socket.send(JSON.stringify({
+				"purpose": "my_name",
+				"data": {
+					"name": Date.now()
+				}
+			}))
+			break;
+		case "my_name":
+			console.log("Other player name:", msg['data']['name'])
+			break;
+	}
 });
--- server.js
+++ server.js
@@ -20,63 +20,48 @@ wss.on('connection', (ws) => {
 	console.log("[ WS ]   New connection")
 
 	// add client to database
-	// var uniq_id = Date.now()
-	var uniq_id = idd++;
-	clients.push({ con: ws, stat: undefined, id: uniq_id })
-	ws.send(uniq_id)
+	var current_id = idd++; // Date.now()
+	clients.push({ con: ws, stat: undefined, id: current_id })
+	ws.send(current_id)
 
-	// check if game available for client
-	// if yes, give them stat
-	var p1 = clients.find(client => client.stat == undefined && client.id != uniq_id)
-	if (p1 != undefined) {
-		console.log("[ GAME ] Match between:" + uniq_id + ' ' + p1.id + ' game:' + games)
-		var p2 = clients[clients.length - 1]
+	// check if game available for client, if yes, give them stat
+	var p1 = clients[clients.length - 1]
+	var p2 = clients.find(client => client.stat == undefined && client.id != current_id)
+	if (p2 != undefined) {
+		console.log("[ GAME ] Match between:" + current_id + ' ' + p2.id + ' game:' + games)
 		p1.stat = games;
 		p2.stat = games;
-		p1['con'].send(JSON.stringify({ "you_are": 'X' }))
-		p2['con'].send(JSON.stringify({ "you_are": 'O' }))
+		p1['con'].send(JSON.stringify({ "purpose": "game_found", "data": { "you_are": "X", "game": games } }))
+		p2['con'].send(JSON.stringify({ "purpose": "game_found", "data": { "you_are": "O", "game": games } }))
 		games++
 	}
 
 	ws.on('error', console.error)
 	ws.on('message', (data) => {
 		data = JSON.parse(data.toString())
-		if (data['i_play'] != undefined) {
 
+		var p1 = clients.find(client => client.con == ws)
+		var p2 = clients.find(client => client.stat == p1.stat && client.id != p1.id)
+		switch (data['purpose']) {
+			case "my_name":
+				p2.con.send(JSON.stringify({ "purpose": "my_name", "data": { "name": data['data']['name'] } }))
+				break;
+			case "i_play":
+				p2.con.send(JSON.stringify({ "purpose": "i_play", "data": { "pos": data['data']['pos'] } }))
+				break;
 		}
 	})
 
 	ws.on('close', _ => {
 		console.log("[ WS ]   A connection was closed")
 
-		// remove connected client and partnet
+		// remove connected client and parter
 		var p1 = clients.find(client => client.con == ws)
-		console.log("[ GAME ] game ended for", p1['id'])
-
-		var p2 = clients.find(client => client.stat == p1['stat'])
+		var p2 = clients.find(client => client.stat == p1['stat'] && client.id != p1.id)
 		if (p2 != undefined)
 			p2.con.close()
 
+		console.log("[ GAME ] Game ended for", p1['id'])
 		clients.splice(clients.indexOf(p1), 1);
 	})
 })
-
-function findFriendFor(wss, uniq_id) {
-	if (clients.length > 1 && clients[clients.length - 2].stat == undefined)
-		return clients[clients.length - 2]
-	return undefined
-}
-
-function getClient(ident, parm = "id") {
-	for (var i in clients) {
-		if (clients[i][parm] == ident) {
-			return clients[i]
-		}
-	}
-	return undefined
-}
-
-function printAll() {
-	for (var i in clients)
-		console.log(clients[i].id, clients[i].stat)
-}
